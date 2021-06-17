a5f080813dd80350523757b6470ab99cf374d1ac
Jvism <jvi.smo@gmail.com>
2021-06-17 18:08:58 -0400

    add_movies_page

---
+++ content/info.js
@@ -0,0 +1,61 @@
+data_movies = [
+    {
+        id:'a1',
+        name:''
+    },
+    {
+
+    }
+]
+
+data_series = [
+    {
+        id:'b1',
+        name:'Peaky blinders',
+        platform: ['Netflix'],
+        img: 'https://occ-0-3832-3934.1.nflxso.net/dnm/api/v6/ifCRgpXJMAE_puF509EDR7a7mqU/AAAABbCbNYV6kDlZR9OWGIQal_EMRhaGXr9fkDzMCWUF4RqKHXhXPH8ZOWJS341JF5enDll8O8EH7_sg8w5aeqZr9TDATHHDCsADznG69WB1hxSpab_wKxSE4Y7CA3OOdYeAy3Hqm7JXzAv-GCXXZHOBneHcUhRjuaIInzZ_5qAvevqch_CZEzhGsmoDX9G7E7e2OVye3ovx.webp?r=851',
+        link: 'https://www.netflix.com/browse?jbv=80002479',
+        trailer: 'https://youtu.be/ZHZG6UArE3I',
+        img_title: 'https://occ-0-3832-3934.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABf8A7SSaLaJ5dfShRQdPvjcFqLsBJMY073uBKM4z5TNElRuaGo4BmJFlgmwGQMcXRG09tYCu8uEUkgaVPxeJVi6d_WUCpYUOo270_DFTOHfYluhtJhYqRAKUJZlBf_a_7gSUn-QYHjqrs4Xf_h3XgS9VAhfmg7kshkOA6fPkqBRd.png?r=d45'
+    },
+    {
+        id:'b2',
+        name:'Sweet Tooth',
+        platform: ['Netflix'],
+        img: 'https://occ-0-3832-3934.1.nflxso.net/dnm/api/v6/ifCRgpXJMAE_puF509EDR7a7mqU/AAAABeh1TdD1Pe8GscmxJyDl1v8ayo-t9b3ZiE5oXOiHufzDRJQ__WoHKUZOhcq6dBTWt54ifYHfUEhLB158W6MhKkCDi0VKxLNuGVbIEUWdAFA-haP2MqTkOXtT8qAD3HJiKEQqObXwL5ARQrN3u7DziPZXID88RAia7EwwvxYPcM6lPtfqyBFLmQSISLyXnAN0G4Qcto8k5_Jv4ya2iABMmKHZ5DP-dgL1pb0_DZSMCIwHIFv6uRGTdYwVwcR7dKxeBlA.webp?r=296',
+        img2: 'https://occ-0-3832-3934.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABce14uRnxdx-V8HvEIAWGLE-HoAg3mlB1NltyK2YjvLnsaXhSY4xTIUMKvuw019qYduJIMu3uNG9WYaO8bh-jhXZ7-Td.jpg?r=808',
+        link: 'https://www.netflix.com/browse?jbv=81221380',
+        trailer: 'https://youtu.be/0jFtt5Fe5CM',
+        img_title: 'https://occ-0-3832-3934.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABZ4Bq3UdmaMmsVIkl1SkaPmnKC8H9GRMPTXzGLV6nKO7sX_jx9hIrljUulHsWKLRqytrlJB90NtMYDvgBV1jYlwgQKBWPH8qsgHs6Gm89T8KfIkJqT1aOq7oS9pmlXNZUgvR-cXweILC7hWIwNzDSs1C6wzcP9Elx3j887zafbwa.png?r=814'
+    },
+    {
+        id:'b3',
+        name:'Gambito de dama',
+        platform: ['Netflix'],
+        img: 'https://occ-0-3832-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeQ73VGUcIdCYnLTxWTXNARcQmoBuum4iQAoV9bV64zf15crCtETtByXdF8lEpFRfs6xoqgkDAQz5hT3MX43pY9pz8kJAOhRNjpFPJ9ZR5OlofKPxPQqmnnY7fQZ.jpg?r=f55',
+        link: 'https://www.netflix.com/browse?jbv=80002479',
+        trailer: 'https://youtu.be/lbleRbyGKL4',
+        img_title: 'https://occ-0-3832-3934.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABQmTU78hBUjLdkHy6lp8a8YQtG6M_RYnNyy1P97XVMBtvligxIDb4mV_LfwzXhEo7sJoggBGjsYvpnvvqcn8MFi2tNQ8ZLWt1yQ6as8XKj8JQgR5CsM0gI7Ou4trIePE4sbgzn8jstSXbF1cmgXwBgd75CMZ0bFPXQyGjq9FNEL1.png?r=431'
+    },
+    {
+        id:'b4',
+        name:'',
+        platform: [''],
+        img: '',
+        link: ''
+    },
+    {
+        id:'b5',
+        name:'',
+        platform: [''],
+        img: '',
+        link: ''
+    },
+    {
+        id:'b6',
+        name:'',
+        platform: [''],
+        img: '',
+        link: ''
+    }
+]
